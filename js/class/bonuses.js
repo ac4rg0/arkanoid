90a7f3fb30ee429606369cbfc5d69c7fd8ea883a
Adrián <adriancarenas@gmail.com>
2016-05-11 21:14:14 +0000

    Sprite attribute remove because inherited from elements.js

--- js/class/bonuses.js
+++ js/class/bonuses.js
@@ -1,7 +1,7 @@
 //function Bonus(x, y, sprite, v, type, width, height) {
 function Bonus(x,y, type, sprite ,width, height) {
   this.type = type;
-  this.sprite = sprite;
+  //this.sprite = sprite;
   this.w = width;
   this.h = height;
   MovElement.apply(this, [x, y, sprite, 80]);
